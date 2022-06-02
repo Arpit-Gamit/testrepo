dd1bc72f89b243648e43da66595f6ad835e72c18
Arpit-Gamit <106726015+Arpit-Gamit@users.noreply.github.com>
2022-06-02 11:25:48 +0530

    Add files via upload

---
+++ pr1a.py
@@ -0,0 +1,6 @@
+''' Description --> we Have to write a program checking that passing character is vowel or not'''
+l=str(input('Enter a Character:'))
+if l=='a' or l=='e' or l=='i' or l=='o' or l=='u' or l=='A' or l=='E' or l=='I' or l=='O' or l=='U':
+	print('Passing Character is Vowel !!')
+else:
+	print('passing character is not a vowel !!')
